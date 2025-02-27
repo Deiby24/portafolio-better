460cb6a1e4604689419706449419e271b2a1688a
Deiby <deiby.8@hotmail.es>
2025-02-27 11:39:01 -0500

    fix skills  responsive

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -8,18 +8,15 @@ const Skills = () => {
     return (
         <div className="relative w-full py-24 bg-transparent">
             <h2 className="text-4xl md:text-5xl font-bold text-center mb-16 text-white">
-                SKILLS & EXPERTISE
+                SKILLS
             </h2>
 
             <div className="relative flex flex-wrap justify-center items-center gap-6 px-4 md:px-12">
-                {skillsData.map((item, index) => (
+                {skillsData.map((item) => (
                     <motion.div
-                        key={item.id}
-                        className="relative w-64 h-116  bg-white/10 backdrop-blur-lg border border-white/20 rounded-2xl shadow-lg overflow-hidden transform transition-transform duration-300"
-                        style={{
-                            transform: `rotate(${index % 2 === 0 ? "-4deg" : "4deg"}) translateY(${index * 5}px)`,
-                        }}
-                        whileHover={{ scale: 1.05, rotate: 0 }}
+                    key={item.id}
+                    className="relative w-full max-w-xs bg-white/10 backdrop-blur-lg border border-white/20 rounded-2xl shadow-lg overflow-hidden transform transition-transform duration-300"
+                    whileHover={{ scale: 1.05, rotate: 0 }}
                     >
                         <div className="w-full h-48 overflow-hidden">
                             <motion.img
